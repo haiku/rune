c6e6d2749eda804764a89227965d32cd368eb930
Alexander von Gluck IV <kallisti5@unixzen.com>
2017-07-26 13:20:38 -0500

    image_tools: Implement write func

--- src/image_tools/mod.rs
+++ src/image_tools/mod.rs
@@ -8,9 +8,12 @@
  *   Alexander von Gluck IV <kallisti5@unixzen.com>
  */
 
- use apperror::AppError;
- use std::path::PathBuf;
+use apperror::AppError;
+use std::path::PathBuf;
+use std::io;
+use std::fs;
 
- pub fn write(source: PathBuf, dest: PathBuf) {
-	 	println!("Writing {:?} to {:?}...", source, dest);
- }
+/// Write file at source to dest
+pub fn write(source: PathBuf, dest: PathBuf) -> io::Result<u64> {
+	return fs::copy(source.as_path(), dest.as_path());
+}
